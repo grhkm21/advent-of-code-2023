456cc2da07cc93aaf82edf1dfd793c545a42c88c
Gareth Ma <grhkm21@gmail.com>
2023-12-12 05:27:34 +0000

    [python] day 12 (67 / 148)

--- python/q12.py
+++ python/q12.py
@@ -14,6 +14,8 @@ import timeit
 import datetime
 
 from typing import Any
+from colorama import Fore, Style
+from sympy.ntheory.modular import crt, solve_congruence
 from collections import Counter, deque
 from functools import reduce, cache
 from random import random, randrange, randint
@@ -21,17 +23,20 @@ from tqdm import tqdm, trange
 from util import *
 
 
+def get_day():
+    qno = __file__.split("/")[-1]
+    if qno == "template.py":
+        raise RuntimeError("Don't run template.py :)")
+    return int(qno[1:3])
+
+
 def parse_data():
     if len(sys.argv) == 1:
-        qno = __file__.split("/")[-1]
-        if qno == "template.py":
-            raise RuntimeError("The code does not work for template.py :)")
-        else:
-            fname = "../input/in_2023_" + qno[1:3]
+        fname = f"../input/in_2023_{get_day()}"
     elif len(sys.argv) == 2:
         fname = sys.argv[1]
     else:
-        raise RuntimeError(f"Usage: {sys.argv[0]} [input]")
+        raise RuntimeError(f"Usage: {sys.argv[0]} [input_file]")
 
     global data
     with open(fname, "r") as fin:
@@ -42,8 +47,37 @@ def solve():
     part1 = 0
     part2 = 0
 
-    for line in data:
-        pass
+    @cache
+    def _solve(s, nums, prev=0):
+        if len(s) == 0:
+            return len(nums) == 0 or (len(nums) == 1 and prev == nums[0])
+        c = s[0]
+        if c == "?":
+            return _solve("." + s[1:], nums, prev) + _solve("#" + s[1:], nums, prev)
+        if c == ".":
+            if len(nums) == 0:
+                return _solve(s[1:], nums, 0)
+            if prev == 0:
+                return _solve(s[1:], nums, 0)
+            if prev != nums[0]:
+                return 0
+            return _solve(s[1:], nums[1:], 0)
+        if c == "#":
+            if len(nums) == 0:
+                return 0
+            if prev + 1 > nums[0]:
+                return 0
+            return _solve(s[1:], nums, prev + 1)
+        raise RuntimeError(f"_solve({s}, {nums}, {prev})")
+
+    def solve(s, nums):
+        return _solve(s, tuple(nums), 0)
+
+    for line in tqdm(data):
+        pat, nums = line.split(" ")
+        nums = list(map(int, nums.split(",")))
+        part1 += solve(pat, nums)
+        part2 += solve("?".join([pat] * 5), nums * 5)
 
     return (part1, part2)
 
@@ -56,13 +90,30 @@ def main(file=sys.stdout):
 
 if __name__ == "__main__":
     parse_data()
-    arg = os.environ.get("TIME")
+    time_arg = os.environ.get("TIME")
+    submit_arg = os.environ.get("SUBMIT")
 
-    if arg is not None:
+    if submit_arg is not None:
+        from submit import submit
         try:
-            arg = int(arg)
+            arg = int(submit_arg)
         except ValueError:
-            raise ValueError(f"The `TIME` environment variable ({arg}) is not an integer.")
+            raise ValueError(f"The `SUBMIT` environment variable ({submit_arg}) is not an integer.")
+
+        if not (0 <= arg <= 2):
+            raise ValueError(f"Invalid `SUBMIT` ({submit_arg}), should be between 0 and 2.")
+
+        part1, part2 = solve()
+        part = 2 if arg == 2 or (arg == 0 and part2 != 0) else 1
+        ans = part2 if arg == 2 or (arg == 0 and part2 != 0) else part1
+        print(f"Submitting {Fore.GREEN}{ans}{Style.RESET_ALL} to day {get_day()} part {part}.")
+        submit(part, ans, get_day(), 2023)
+
+    elif time_arg is not None:
+        try:
+            arg = int(time_arg)
+        except ValueError:
+            raise ValueError(f"The `TIME` environment variable ({time_arg}) is not an integer.")
 
         print(f"Timing code for {arg} times!")
         with open(os.devnull, "w") as fout:
