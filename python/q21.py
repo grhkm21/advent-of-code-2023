6ef4099529c92e96376d49d34d22c78ffd8a7e9b
Gareth Ma <grhkm21@gmail.com>
2023-12-26 01:43:00 +0000

    [python] day 21 (96 / 456)

--- python/q21.py
+++ python/q21.py
@@ -12,11 +12,14 @@ import sys
 import time
 import timeit
 import datetime
+import sympy
+import sympy.abc
 
-from typing import Any
+from fractions import Fraction
+from typing import Any, Deque, Tuple
 from colorama import Fore, Style
 from sympy.ntheory.modular import crt, solve_congruence
-from collections import Counter, deque
+from collections import Counter, defaultdict, deque
 from functools import reduce, cache
 from random import random, randrange, randint
 from tqdm import tqdm, trange
@@ -47,8 +50,48 @@ def solve():
     part1 = 0
     part2 = 0
 
-    for line in data:
-        pass
+    r, c = len(data), len(data[0])
+    assert r == c
+
+    sx, sy = 0, 0
+    dq = deque()
+    for i, j in itertools.product(range(r), range(c)):
+        if data[i][j] == "S":
+            sx, sy = i, j
+            dq.append((sx, sy, 0))
+            break
+    else:
+        raise RuntimeError("S not found")
+
+    target = 5000
+    offset = target % r
+
+    vis = set([(sx, sy)])
+    dist = {}
+    while len(dq) > 0:
+        x, y, d = dq.popleft()
+        dist[x, y] = d
+        if d > offset + 5 * r:
+            continue
+        for nx, ny in adj4(x, y):
+            if (nx, ny) not in vis and data[nx % r][ny % c] != "#":
+                vis.add((nx, ny))
+                dq.append((nx, ny, d + 1))
+
+    def get_val(st):
+        return len([dist[x, y] for x, y in dist if dist[x, y] <= st and dist[x, y] % 2 == st % 2])
+
+    def interpolate(xs, f, nx):
+        n = len(xs)
+        ys = [f(x) for x in xs]
+        res = 0
+        for i in range(n):
+            cur = product((sympy.abc.x - xs[j]) / (xs[i] - xs[j]) for j in range(n) if j != i)
+            res += cur * ys[i]
+        return res.subs({sympy.abc.x: nx})
+
+    part1 = get_val(64)
+    part2 = interpolate(range(3), lambda x: get_val(offset + r * x), target // r)
 
     return (part1, part2)
 
