856db28e33a15600062c8858e84b4270b23ba9b4
Gareth Ma <grhkm21@gmail.com>
2023-12-26 01:43:13 +0000

    [python] day 22 (51 / 86)

--- python/q22.py
+++ python/q22.py
@@ -12,11 +12,12 @@ import sys
 import time
 import timeit
 import datetime
+import copy
 
 from typing import Any
 from colorama import Fore, Style
 from sympy.ntheory.modular import crt, solve_congruence
-from collections import Counter, deque
+from collections import Counter, defaultdict, deque
 from functools import reduce, cache
 from random import random, randrange, randint
 from tqdm import tqdm, trange
@@ -40,15 +41,79 @@ def parse_data():
 
     global data
     with open(fname, "r") as fin:
-        data = fin.read().strip().split("\n")
+        data = []
+        for line in fin.read().strip().split("\n"):
+            a, b = map(lambda s: list(map(int, s.split(","))), line.split("~"))
+            data.append((a, b))
 
 
 def solve():
     part1 = 0
     part2 = 0
 
-    for line in data:
-        pass
+    def make_piece(p, q):
+        return list(itertools.product(*map(lambda r: range(r[0], r[1] + 1), map(sorted, zip(p, q)))))
+
+    def drop(piece):
+        return [(x, y, z - 1) for x, y, z in piece]
+
+    pieces = [make_piece(a, b) for a, b in data]
+    assert all((0, 0, 0) <= p < (10, 10, 300) for piece in pieces for p in piece)
+
+    grid = np.zeros((10, 10, 300), dtype=int)
+    for pos in itertools.product(range(10), range(10), range(300)):
+        grid[pos] = -1
+
+    for i, piece in enumerate(pieces):
+        for p in piece:
+            grid[p] = i
+
+    while True:
+        flag = False
+        for i, piece in enumerate(pieces):
+            for p in piece:
+                grid[p] = -1
+            new_piece = drop(piece)
+            if all(p[2] >= 1 for p in new_piece) and all(grid[p] == -1 for p in new_piece):
+                flag = True
+                pieces[i] = new_piece
+            for p in pieces[i]:
+                grid[p] = i
+        if not flag:
+            break
+
+    dt = []
+    rev = defaultdict(set)
+    for i, piece in enumerate(pieces):
+        new_piece = drop(piece)
+        cur = set()
+        for j, p in enumerate(new_piece):
+            if p[2] >= 1 and grid[p] != -1 and grid[p] != i:
+                cur.add(grid[p])
+                rev[grid[p]].add(i)
+        dt.append(cur)
+
+    for i in range(len(pieces)):
+        for j in range(len(pieces)):
+            if j == i:
+                continue
+            if len(dt[j]) == 1 and i in dt[j]:
+                break
+        else:
+            part1 += 1
+
+    def dfs(v):
+        nonlocal gone
+        gone.add(v)
+        for u in rev[v]:
+            if all(d in gone for d in dt[u]):
+                dfs(u)
+
+    for v in range(len(pieces)):
+        gone = set()
+        dfs(v)
+        # print(v, gone)
+        part2 += len(gone) - 1
 
     return (part1, part2)
 
