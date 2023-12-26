7d0eef272e953f72b832bc1a3b92431b369e6596
Gareth Ma <grhkm21@gmail.com>
2023-12-26 01:43:24 +0000

    [python] day 23 (652 / 959)

--- python/q23.py
+++ python/q23.py
@@ -1,3 +1,4 @@
+import copy
 import glob
 import hashlib
 import itertools
@@ -12,15 +13,18 @@ import sys
 import time
 import timeit
 import datetime
+import networkx as nx
 
 from typing import Any
 from colorama import Fore, Style
 from sympy.ntheory.modular import crt, solve_congruence
-from collections import Counter, deque
+from collections import Counter, defaultdict, deque
 from functools import reduce, cache
-from random import random, randrange, randint
+from random import choice, random, randrange, randint
 from tqdm import tqdm, trange
 from util import *
+
+sys.setrecursionlimit(10**5)
 
 
 def get_day():
@@ -47,8 +51,77 @@ def solve():
     part1 = 0
     part2 = 0
 
-    for line in data:
-        pass
+    graph1 = defaultdict(dict)
+    graph2 = defaultdict(dict)
+    r, c = len(data), len(data[0])
+
+    START = (0, data[0].index("."))
+    END = (r - 1, data[r - 1].index("."))
+
+    is_valid = lambda x, y: 0 <= x < r and 0 <= y < c and data[x][y] != "#"
+    ndir_dict = {">": [(0, 1)], "<": [(0, -1)], "^": [(-1, 0)], "v": [(1, 0)], ".": list(adj4(0, 0))}
+
+    for i, j in itertools.product(range(r), range(c)):
+        if data[i][j] == "#":
+            continue
+
+        for ndir in ndir_dict[data[i][j]]:
+            x, y = i + ndir[0], j + ndir[1]
+            if is_valid(x, y):
+                graph1[i, j][x, y] = 1
+                if data[x][y] == ".":
+                    graph1[x, y][i, j] = 1
+
+        for x, y in adj4(i, j):
+            if is_valid(x, y):
+                graph2[i, j][x, y] = 1
+                graph2[x, y][i, j] = 1
+
+    while True:
+        for u in graph2:
+            if len(graph2[u]) == 2:
+                left, right = list(graph2[u].keys())
+                weight = sum(graph2[u].values())
+                graph2[left][right] = weight
+                graph2[right][left] = weight
+                del graph2[u]
+                del graph2[left][u]
+                del graph2[right][u]
+                break
+        else:
+            break
+
+    def solve_graph(graph):
+        # Reindex graph vertices
+        keys = {y: x for x, y in enumerate(graph.keys())}
+        graph = {keys[u]: {keys[v]: graph[u][v] for v in graph[u]} for u in graph}
+
+        ans = 0
+        _START, _END = keys[START], keys[END]
+        dq = deque()
+
+        dq.append((_START, 1 << _START, 0))
+        while len(dq) > 0:
+            u, vis, dist = dq.pop()
+            if u == _END:
+                ans = max(ans, dist)
+                continue
+            while len(d := graph[u]) == 1:
+                u, dt = list(d.items())[0]
+                if (vis >> u) & 1:
+                    break
+                vis |= 1 << u
+                dist += dt
+            else:
+                for v, dt in d.items():
+                    if (vis >> v) & 1:
+                        continue
+                    dq.append((v, vis | (1 << v), dist + dt))
+
+        return ans
+
+    part1 = solve_graph(graph1)
+    part2 = solve_graph(graph2)
 
     return (part1, part2)
 
